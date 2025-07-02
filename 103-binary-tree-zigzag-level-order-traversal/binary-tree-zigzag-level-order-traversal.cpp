87a85f3a4b29722bdc79fc0fd7ac3c8e92286f9f
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-07-02 12:37:22 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (98.46%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,32 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if (root == nullptr) return result;
-
-        queue<TreeNode*> q;
-        q.push(root);
-        bool leftToRight = true;
-
-        while (!q.empty()) {
-            int size = q.size();
-            vector<int> ans(size);
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-
-                int index = leftToRight ? i : (size - 1 - i);
-                ans[index] = node->val;
-
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-
-            result.push_back(ans);  
-            leftToRight = !leftToRight;  
+        vector<vector<int>> ans;
+        DFS(root, 0, ans);
+        for (int i = 1; i < ans.size(); i += 2) {
+            reverse(ans[i].begin(), ans[i].end());
         }
-
-        return result;
+        return ans;
+    }
+    
+private:
+    void DFS(TreeNode* root, int depth, vector<vector<int>> &ans) {
+        if (!root) return;
+        while (ans.size() <= depth) ans.push_back({});
+        ans[depth].push_back(root->val);
+        DFS(root->left, depth + 1, ans);
+        DFS(root->right, depth + 1, ans);
     }
 };
