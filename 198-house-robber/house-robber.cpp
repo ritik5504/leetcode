97a4c5ea52eb820c9e400513f8157a6bcb164af5
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-07-25 14:19:08 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (90.23%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         if(n==0) return 0;
         if(n==1) return nums[0];
-        if(n==2) return max(nums[0],nums[1]);
+        // if(n==2) return max(nums[0],nums[1]);
 
         int pre2=nums[0];
         int pre1=max(nums[0],nums[1]);
