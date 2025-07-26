afe9fbab246a916ff1e4ba3cf790b2fbc4a421a0
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-07-26 15:49:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.40%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-
     int solve(vector<int>& nums,int start,int end){
         int pre1=0,pre2=0;
+
         for(int i=start;i<end;i++){
-            int curr=max(pre1,nums[i]+pre2);
+             int curr=max(nums[i]+pre2,pre1);
             pre2=pre1;
             pre1=curr;
+
+
         }
         return pre1;
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if(n==0) return 0;
+        if(n==0)return 0;
         if(n==1) return nums[0];
 
         int case1=solve(nums,0,n-1);
         int case2=solve(nums,1,n);
         return max(case1,case2);
-
-
         
     }
 };
