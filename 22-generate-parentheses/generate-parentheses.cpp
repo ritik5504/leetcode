421738ec1b984fa15a911663d054abeef60f7f5b
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-07-28 14:10:14 +0530

    Time: 1 ms (87.21%) | Memory: 10.4 MB (98.51%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    void solve(int oc, int cc, string curr, vector<string>& ans, int n) {
-        if (oc == n && cc == n) {
-            ans.push_back(curr);
-            return;
-        }
-
-        if (oc < n) {
-            solve(oc + 1, cc, curr + '(', ans, n);
-        }
-
-        if (cc < oc) {
-            solve(oc, cc + 1, curr + ')', ans, n);
-        }
-    }
-
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        int oc = 0, cc = 0;
-        string curr = "";
-        solve(oc, cc, curr, ans, n);  
-        return ans;
+        vector<vector<string>> dp(n + 1);
+        dp[0]={""};
+
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                for(string in:dp[j]){
+                    for(string out:dp[i-1-j]){
+                        dp[i].push_back("("+in+")"+out);
+                    }
+                }
+            }
+        }
+        return dp[n];
+        
     }
 };
