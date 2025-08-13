901477764c9c9bb9beb5b95f7e1f2210f4b149eb
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-08-13 14:09:15 +0530

    Time: 175 ms (20.23%) | Memory: 110.6 MB (33.12%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
+    bool solve(int idx,vector<int>& nums,int n,int target,vector<vector<int>>&dp){
+        if(target < 0) return 0;
+        if(idx>= n) return 0;
+        if(target == 0) return 1;
+
+        if(dp[idx][target] != -1) return dp[idx][target];
+
+        int inc=solve(idx+1,nums,n,target-nums[idx],dp);
+        int exc=solve(idx+1,nums,n,target,dp);
+
+        return dp[idx][target]=inc or exc;
+    }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
-        int sum=0;
-
+        int total =0;
         for(int i=0;i<n;i++){
-            sum += nums[i];
+            total += nums[i];
         }
-        if(sum % 2 != 0){
-            return false;
-        }
-        int target=sum/2;
-        vector<bool>dp(target+1,false);
-        dp[0]=true;
-        for(int num:nums){
-            for(int j=target;j>=num;j--){
-                dp[j]=dp[j]||dp[j-num];
-            }
+        if(total % 2 != 0) return 0;
+        int target=total/2;
 
-        }
-        return dp[target];
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
+
+        return solve(0,nums,n,target,dp);
+        
     }
 };
