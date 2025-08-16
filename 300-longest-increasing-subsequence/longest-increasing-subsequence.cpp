52e1e41d8039c98db97e84a7b439a26b00489814
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-08-17 00:15:41 +0530

    Time: 596 ms (9.05%) | Memory: 296.2 MB (34.82%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int solve(int curr,int pre,vector<int>& nums,vector<vector<int>>&dp){
         if(curr==nums.size()) return 0;
-
         if(dp[curr][pre+1] != -1) return dp[curr][pre+1];
 
         int take=0;
-        if(pre == -1 || nums[curr] > nums[pre]){
+        if(pre==-1 || nums[curr]>nums[pre]){
             take=1+solve(curr+1,curr,nums,dp);
         }
         int skip=solve(curr+1,pre,nums,dp);
@@ -17,7 +16,8 @@ public:
         
         int n=nums.size();
         vector<vector<int>>dp(n,vector<int>(n+1,-1));
-
         return solve(0,-1,nums,dp);
+
     }
 };
+
