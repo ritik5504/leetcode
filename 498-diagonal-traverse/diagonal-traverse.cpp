4ee2adc47edb23c7feb14ca8b31f745fdcfd091e
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-08-25 11:49:15 +0530

    Time: 11 ms (47.52%) | Memory: 27.1 MB (34.65%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> dia(m + n - 1);
+
+        // Loop over rows (i) and columns (j)
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                dia[i + j].push_back(mat[i][j]);
+            }
+        }
+
+        vector<int> ans;
+
+        // Collect diagonals
+        for(int d = 0; d < dia.size(); d++) {
+            if(d % 2 == 0) {
+                reverse(dia[d].begin(), dia[d].end());
+            }
+            for(int val : dia[d]) {
+                ans.push_back(val);
+            }
+        }
+
+        return ans;
+    }
+};
