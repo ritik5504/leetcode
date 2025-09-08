61e5724ecc25e612660f5d8312dd5076b85005b9
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-09-08 19:04:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (85.27%) - LeetSync

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        vector<int> result;
+        if(n % 2 == 0){
+            int st = n/2, end = n/2;
+
+            while(st > 0 && end < n){
+                
+                bool flag1 = true, flag2 = true;
+                int temp = st;
+                while(temp > 0){
+                    int rem = temp%10;
+                    temp /= 10;
+                    if(rem == 0){
+                        flag1 = false;
+                        break;
+                    }
+                }
+
+                temp = end;
+                while(temp > 0){
+                    int rem = temp%10;
+                    if(rem == 0){
+                        flag2 = false;
+                        break;
+                    }
+                    temp /= 10;
+                }
+
+                if(flag1 && flag2){
+                    result.push_back(st);
+                    result.push_back(end);
+                    break;
+                }
+                st--, end++;
+            }
+        } else {
+            int st = n/2, end = n/2+1;
+
+            while(st > 0 && end < n){
+                
+                bool flag1 = true, flag2 = true;
+                int temp = st;
+                while(temp > 0){
+                    int rem = temp%10;
+                    temp /= 10;
+                    if(rem == 0){
+                        flag1 = false;
+                        break;
+                    }
+                }
+
+                temp = end;
+                while(temp > 0){
+                    int rem = temp%10;
+                    if(rem == 0){
+                        flag2 = false;
+                        break;
+                    }
+                    temp /= 10;
+                }
+
+                if(flag1 && flag2){
+                    result.push_back(st);
+                    result.push_back(end);
+                    break;
+                }
+                st--, end++;
+            }
+        }
+
+        return result;
+    }
+};
