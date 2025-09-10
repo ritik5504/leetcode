c5e439f73a119b59c039bde271304dfb06fc3de2
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-09-10 10:40:17 +0530

    Time: 41 ms (15.05%) | Memory: 31.3 MB (40.10%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -5,29 +5,27 @@ public:
         if(i>=0 && j<0) return false;
         if(i<0 && j>=0){
             for(int k=0;k<=j;k++){
-                if(p[k] != '*'){
+                if(p[k] !='*'){
                     return false;
                 }
             }
             return true;
         }
-
         if(dp[i][j] != -1) return dp[i][j];
-        if(s[i]==p[j] || p[j] == '?'){
+
+        if(s[i]==p[j] || p[j]=='?'){
             return dp[i][j]=solve(s,p,i-1,j-1,dp);
-        }else if(p[j]=='*'){
-            return dp[i][j] = (solve(s,p,i-1,j,dp)||solve(s,p,i,j-1,dp));
-           
-        }else{
+        }else if(p[j] == '*'){
+            return dp[i][j]=(solve(s,p,i-1,j,dp)|| solve(s,p,i,j-1,dp));
+        }
+        else{
             return false;
         }
-
     }
     bool isMatch(string s, string p) {
         int n=s.length();
         int m=p.length();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-
         return solve(s,p,n-1,m-1,dp);
         
     }
