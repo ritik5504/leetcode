35fe8e3c6ea087971d2e3f4a36001a6c74e31fcf
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-09-12 11:14:15 +0530

    Time: 8 ms (52.24%) | Memory: 19.1 MB (53.73%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        for(char ch:s){
+            if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'){
+                return true;
+            
+            }
+        }
+        return false;
+        
+    }
+};
