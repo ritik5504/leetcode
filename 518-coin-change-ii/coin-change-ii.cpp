9467d74be4eb0bc1d09da46d490c89c7f81c956b
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-09-12 20:26:03 +0530

    Time: 31 ms (27.65%) | Memory: 28.6 MB (40.86%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -4,25 +4,21 @@ public:
         if(i==coins.size()){
             if(amount==0) return 1;
             else{
-                return 0;
+                return false;
             }
-
         }
-
         if(dp[i][amount] != -1) return dp[i][amount];
-
         int skp=solve(i+1,amount,coins,dp);
         int take=0;
         if(coins[i]<=amount){
             take=solve(i,amount-coins[i],coins,dp);
-
         }
-        return dp[i][amount]=take+skp;
+        return dp[i][amount] = take+skp;
     }
     int change(int amount, vector<int>& coins) {
-        
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
         return solve(0,amount,coins,dp);
+        
     }
 };
