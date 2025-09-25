b13d61e2adfab232ae7d6b69497fdb26c3893f29
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-09-25 11:55:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (66.61%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<int>dp=triangle[n-1];
+        vector<int>dp(triangle.back());
 
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<=i;j++){
