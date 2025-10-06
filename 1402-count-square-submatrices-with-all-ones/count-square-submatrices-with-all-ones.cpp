dc69f2033ea843570b39f687c3af8612e94da11a
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-10-06 13:14:08 +0530

    Time: 15 ms (18.74%) | Memory: 31.6 MB (5.73%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
+    int solve(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>& dp){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(matrix[i][j]==0) return 0;
+
+        int top=solve(i-1,j,matrix,dp);
+        int left=solve(i,j-1,matrix,dp);
+        int dio=solve(i-1,j-1,matrix,dp);
+
+        return dp[i][j]= 1+min({top,left,dio});
+
+
+    }
     int countSquares(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int n=matrix.size(),m=matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
 
-        int cnt = 0;
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-
-        // Initialize first row and column
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = matrix[i][0];
-        }
-        for (int j = 0; j < m; j++) {
-            dp[0][j] = matrix[0][j];
-        }
-
-        // Fill DP table
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][j] == 1) {
-                    dp[i][j] = 1 + min({dp[i - 1][j - 1],
-                                        dp[i - 1][j], 
-                                        dp[i][j - 1]});
-                }
+        int total=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                total += solve(i,j,matrix,dp);
             }
         }
-
-        // Count all squares
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                cnt += dp[i][j];
-            }
-        }
-
-        return cnt;
+        return total;
+        
     }
 };
