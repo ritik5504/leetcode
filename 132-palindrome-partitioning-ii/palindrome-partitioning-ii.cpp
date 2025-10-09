0fabe6f572cef16d882427f01dbe884f7a5a4544
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-10-09 09:59:11 +0530

    Time: 1758 ms (15.69%) | Memory: 9.4 MB (63.92%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool ispal(string& s,int l,int r){
+        while(l<r){
+            if(s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    int solve(int i,string& s,vector<int>&dp){
+        int n=s.size();
+        if(i==n) return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int ans=INT_MAX;
+        for(int j=i;j<n;j++){
+            if(ispal(s,i,j)){
+                if(j==n-1)ans= 0;
+                else{
+                    ans=min(ans,1+solve(j+1,s,dp));
+                }
+            }
+        }
+        return dp[i]=ans;
+    }
+    int minCut(string s) {
+        int n=s.size();
+        vector<int>dp(n,-1);
+
+        return solve(0,s,dp);
+        
+    }
+};
