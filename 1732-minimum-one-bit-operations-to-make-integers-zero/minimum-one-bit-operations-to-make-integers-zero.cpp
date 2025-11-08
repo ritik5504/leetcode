47e85a482ee1748d2c48f5ccabc6eeb6f1a4d08b
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-11-08 13:14:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (80.63%) - LeetSync

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        
+        int ans=0;
+
+        while(n){
+            ans^=n;
+            n>>=1;
+        }
+        return ans;
+    }
+};
