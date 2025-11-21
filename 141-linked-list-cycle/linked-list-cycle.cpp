47272b9081d4a0b28acbf3c7bebc4f3addf302a8
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-11-21 15:11:20 +0530

    Time: 12 ms (38.00%) | Memory: 11.7 MB (79.19%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -12,16 +12,13 @@ public:
         ListNode* slow=head;
         ListNode* fast=head;
 
-        while(fast != nullptr && fast->next != nullptr){
+        while(fast!=nullptr && fast->next != nullptr){
             slow=slow->next;
             fast=fast->next->next;
 
-            if (slow==fast){
-                return true;
-            }
-        } 
+            if(slow==fast)return true;
+        }
         return false;
-
         
     }
 };
