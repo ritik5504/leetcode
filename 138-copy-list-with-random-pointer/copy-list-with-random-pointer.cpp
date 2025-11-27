3feba0e58fec4825d603953b1dd0a7b3da730c83
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2025-11-27 15:10:53 +0530

    Time: 4 ms (68.96%) | Memory: 14.9 MB (96.18%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*, Node*>mp;
+        Node* temp=head;
+
+        while(temp != NULL){
+            Node* copy=new Node(temp->val);
+            mp[temp]=copy;
+            temp=temp->next;
+        }
+        temp=head;
+        while(temp != NULL){
+            Node* copy=mp[temp];
+            copy->next=mp[temp->next];
+            copy->random=mp[temp->random];
+            temp=temp->next;
+        }
+        return mp[head];
+        
+    }
+};
